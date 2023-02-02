e783f7f61a2999bc431cff4641671ebeca6e8504
Saquib <saquibparvez007@gmail.com>
2023-02-02 14:53:39 +0530

    portfolio

---
+++ server/src/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const userModel=require('./models/userModel')
+const cors=require('cors')
+app.use(cors({ origin:'https://saquib007.netlify.app/'}));  //'http://localhost:3001' || 'http://localhost:3002'||
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+
+
+
+mongoose.connect("mongodb+srv://saquib007:Jdo1pCEGwZpOgtdD@cluster0.rvsi5lf.mongodb.net/test", {
+    useNewUrlParser: true
+})
+.then(()=> console.log("MongoDB is connected"))
+.catch(err => console.log(err))
+
+app.post('/form-submit', async  (req, res) => {
+try {
+    const data= req.body;
+
+const newuser=await userModel.create(data)
+return res.status(201).send({msg:"success",data:newuser})
+ 
+} catch (error) {
+    return res.status(500).send({msg:error.message})
+
+}    
+      });
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express app running on Port " + (process.env.PORT || 3000))
+})
+
+
+
