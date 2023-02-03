505af40f3999f0e0a3b083a6c8899a5dda861a1a
Saquib <saquibparvez007@gmail.com>
2023-02-03 12:12:14 +0530

    header

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ const userModel=require('./models/userModel')
 const cors=require('cors')
 app.use(bodyParser.json());
 
-app.use(cors({ origin:'https://saquib007.netlify.app/'}));  //'http://localhost:3001' || 'http://localhost:3002'||
+app.use(cors({ origin:'https://saquib007.netlify.app'}));  //'http://localhost:3001' || 'http://localhost:3002'||
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
@@ -20,6 +20,8 @@ mongoose.connect("mongodb+srv://saquib007:Jdo1pCEGwZpOgtdD@cluster0.rvsi5lf.mong
 .catch(err => console.log(err))
 
 app.post('/form-submit', async  (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin','*')
+
 try {
     const data= req.body;
 
@@ -33,6 +35,8 @@ return res.status(201).send({msg:"success",data:newuser})
       });
 
       app.get('/test',async(req,res)=>{
+        res.setHeader('Access-Control-Allow-Origin','*')
+
         return res.status(200).send('fine')
       })
 
